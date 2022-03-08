70c51dc4fe5e85427cab16cdcd4264b177c9f2d8
MartinC <sorry741229@gmail.com>
2022-03-08 21:16:13 +0800

    update version

--- pwd2.py
+++ pwd2.py
@@ -1,12 +1,15 @@
 ans = 'a123456'
 x = 3 #初始機會
 while x > 0 :
+	x = x -1
 	pwd = input('請輸入密碼: ')
 	if pwd == ans:
 		print('登入成功')
 		break
 	else:
-		x = x -1
-		print('密碼錯誤! 還有', x,'次機會')
-print('已輸入超過三次，結束')
+		print('密碼錯誤!')
+		if x > 0:
+			print('還有', x,'次機會')
+		else:
+			print('已輸入超過三次，已鎖定帳號')
 		
